30ca13c6a8daff6b34d4a098d47903c76fef79cf
prozhou <zhoushengzheng@gmail.com>
2024-10-18 08:52:04 +0800

    feat: sth

--- dep.sh
+++ dep.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+ROOT_PATH=$HOME/playground/pinion-gen
 
 function Gen {
     if [ "$1" = "l" ]; then
-        find "$HOME/playgroud/pinion-gen" -name "gen.tpl.ts"
+        find $ROOT_PATH -name "gen.tpl.ts"
     else
-        genFile=`find "$HOME/playgroud/pinion-gen" -name "gen.tpl.ts"| grep $1`
+        genFile=`find $ROOT_PATH -name "gen.tpl.ts"| grep $1`
         echo $genFile
-        node $HOME/playgroud/pinion/packages/pinion/bin/pinion.js $genFile
+        #node $HOME/playground/pinion/packages/pinion/bin/pinion.js $genFile
+        npx pinion $genFile
     fi
 }
